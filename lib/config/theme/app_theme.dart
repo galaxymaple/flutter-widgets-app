0fb2fc05687bc3bd477a4f0cc5fabfcf75634fb9
Ryzen <cybermodspace@gmail.com>
2023-06-12 19:54:09 -0400

    Se modifica estilos de AppBar

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.yellow,
+  Colors.purple,
+  Colors.orange,
+  Colors.pink,
+  Colors.teal,
+  Colors.cyan,
+  Colors.lime,
+  Colors.indigo,
+  Colors.amber,
+  Colors.brown,
+  Colors.grey,
+  Colors.blueGrey,
+  Colors.black,
+];
+
+class AppTheme {
+
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  }): assert(selectedColor >= 0, 'Selected color must be greater then 0'),
+    assert(selectedColor < colorList.length, 'Selected color must be less or equal than ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColor],
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+    ),
+  );
+
+}
