3b873b63bad2c0f513375ee77b93e60bdc8f9a76
aerozenspace <cybermodspace@gmail.com>
2023-08-08 22:39:49 -0400

    Tutorial terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -53,4 +53,10 @@ const appMenuItems = <MenuItem>[
     link: '/ui-controls', 
     icon: Icons.car_rental_outlined
   ),
+  MenuItem(
+    title: 'Introducción a la aplicación', 
+    subTitle: 'Pequeño tutorial introductorio', 
+    link: '/tutorial', 
+    icon: Icons.accessibility_rounded
+  ),
 ];
--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,13 +1,139 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class AppTutorialScreen extends StatelessWidget {
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+
+final slides = <SlideInfo>[
+  SlideInfo('Busca la comida', 'Enim et adipisicing aliquip incididunt non magna aute deserunt qui mollit.', 'assets/images/1.png'),
+  SlideInfo('Entrega la comida', 'Amet id ut voluptate amet cupidatat est eu cillum sint velit officia.', 'assets/images/2.png'),
+  SlideInfo('Disfruta la comida', 'Proident veniam incididunt cillum ea magna veniam id reprehenderit est irure dolor officia.', 'assets/images/3.png'),
+];
+
+
+
+class AppTutorialScreen extends StatefulWidget {
   
   static const String name = 'tutorial_screen';
 
   const AppTutorialScreen({super.key});
 
   @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+
+  final PageController pageviewController = PageController();
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    pageviewController.addListener(() {
+
+      final page = pageviewController.page ?? 0;
+      if( !endReached && page >= (slides.length - 1.5) ){
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    pageviewController.dispose();
+    super.dispose();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            controller: pageviewController,
+            physics: const BouncingScrollPhysics(),
+            // Iterable, se espera una lista
+            children: slides.map((slideData) => _Slide(
+              title: slideData.title,
+              caption: slideData.caption,
+              imageUrl: slideData.imageUrl,
+            )).toList(),
+          ),
+          Positioned(
+            right: 20,
+            top: 50,
+            child: TextButton(
+              child: const Text('Salir'),
+              onPressed: () => context.pop(),
+            ),
+          ),
+
+          endReached ? Positioned(
+            bottom: 30,
+            right: 30,
+            child: FadeInRight(
+              from: 15,
+              delay: const Duration(seconds: 1),
+              child: FilledButton(
+                onPressed: () => context.pop(),
+                child: const Text('Comenzar'),
+              ),
+            ),
+            // Sizebox = widget de 0 píxeles
+          ): const SizedBox(),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+
+  // va a recibir la información de cada slide
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(image: AssetImage(imageUrl)),
+            const SizedBox(height: 20),
+            Text(title, style: titleStyle),
+            const SizedBox(height: 20),
+            Text(caption, style: captionStyle)
+          ],
+        ),
+      ),
+    );
   }
 }
